77aacc1bce99977fb82f5d21aae72db8a8292f84
Bianca SchoenPhelan <bianca.schoenphelan@soc-mbp13-bsp.local>
2019-09-24 18:35:15 +0100

    original

---
+++ lab1_dynamic_typing.py
@@ -0,0 +1,17 @@
+#course: Object-oriented programming, year 2, semester 1
+#academic year: 201819
+#author: B. Schoen-Phelan
+#date: 22-09-2018
+#purpose: Lab 1
+
+my_variable = 10
+while my_variable < 20:
+        i = my_variable
+        if i < 12:
+                my_variable +=1
+                print(my_variable)
+        else:
+                my_vairable = (my_variable + 20)
+                print('new')
+
+
