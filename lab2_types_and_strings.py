510cf62d1750a148b025b2ee7d76e70e1b3e3949
Bianca SchoenPhelan <bianca.schoenphelan@soc-mbp13-bsp.lan>
2019-09-29 19:30:35 +0100

    original lab 2 file

---
+++ lab2_types_and_strings.py
@@ -0,0 +1,64 @@
+#course: Object-oriented programming, year 2, semester 1
+#academic year: 201920
+#author: B. Schoen-Phelan
+#date: 29-09-2019
+#purpose: Lab 2
+
+class Types_and_Strings:
+    def __init__(self):
+        pass
+
+    def play_with_strings(self):
+        # working with strings
+        message = input("Enter your noun: ")
+        print("Originally entered: "+ message)
+
+        # print only first and last of the sentence
+
+
+        # use slice notation
+
+
+        # escaping a character
+
+
+        # find all a's in the input word and count how many there are
+
+
+        # replace all occurences of the character a with the - sign
+        # try this first by assignment of a location in a string and
+        # observe what happens, then use replace()
+
+
+        # printing only characters at even index positions
+
+
+    def play_with_lists(self):
+        message = input("Please enter a whole sentence: ")
+        print("Originally entered: "+ message)
+
+        # hand the input string to a list and print it out
+
+
+        # append a new element to the list and print
+
+
+        # remove from the list in 3 ways
+
+
+        # check if the word cake is in your input list
+
+
+        # reverse the items in the list and print
+
+
+        # reverse the list with the slicing trick
+
+
+        # print the list 3 times by using multiplication
+
+
+
+tas = Types_and_Strings()
+tas.play_with_strings()
+#tas.play_with_lists()
